18473860b88199571e8810aae2996df2f147354a
chanlee <pck886@gmail.com>
2017-11-16 22:47:44 +0900

    cafe24 uwsgi & nginx

--- cranesite/wsgi.py
+++ cranesite/wsgi.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-WSGI config for cranesite project.
-
+WSGI config for mosu project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cranesite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mosu.settings")
 
 application = get_wsgi_application()
