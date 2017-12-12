7c5042ce796ed494bcdc69c6c8e744b93771dca0
chanlee <pck886@gmail.com>
2017-12-12 21:30:45 +0900

    search website

--- homepage/urls.py
+++ homepage/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^cta/$',views.cta, name='cta'),
     url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
         name="project_robots_file"),
+    url(r'^sitemap.xml$', TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml")),
 ]
