c4cee2201e166b161a2511d11660cea6c4ba37fc
chanlee <pck886@gmail.com>
2018-05-24 09:21:10 +0900

    -

--- homepage/admin.py
+++ homepage/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Home, About, EquipmentIntro, Images, Equipments, Phones, CompanyInfo, Cta, ImageBoard
+from .models import Home, About, EquipmentIntro, Images, Equipments, Phones, CompanyInfo, Cta, ImageBoard, BoardImages
 
 
 class BoardImagesInline(admin.TabularInline):
